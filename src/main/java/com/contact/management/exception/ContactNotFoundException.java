5363411bfa5dc47b69468966c417e24dc8024733
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-14 17:43:57 +0200

    exception

---
+++ src/main/java/com/contact/management/exception/ContactNotFoundException.java
@@ -0,0 +1,14 @@
+package com.contact.management.exception;
+
+public class ContactNotFoundException extends RuntimeException {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ContactNotFoundException(String message) {
+        super(message);
+    }
+
+
+}
